26b74c54ae60754e3247d294dd074133a5e9f1db
xinjie@elpuig.xeill.net <xinjie@elpuig.xeill.net>
2024-04-29 17:17:58 +0200

    S'han creat 4 metodes nous.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        method1();
+        method2();
+        method3();
+        method4();
     }
+    
+    private static void method1() {}
+    private static void method2() {}
+    private static void method3() {}
+    private static void method4() {}
+    
 }
